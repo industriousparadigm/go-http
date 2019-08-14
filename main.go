9de178ad04ee2c1fba7a57279f4d49d4c9b0d569
mrsalvini <dsgmcosta@gmail.com>
2019-08-14 19:18:41 +0100

    prints google's html

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("http://google.com")
@@ -13,6 +16,19 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(resp)
+	lw := logWriter{}
 
+	io.Copy(lw, resp.Body)
+
+	// bs := make([]byte, 99999)
+
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
+
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	return len(bs), nil
 }
